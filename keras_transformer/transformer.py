498c907770eeb9eab682d058a9bac851071b7580
josephzbao <joseph.z.bao@gmail.com>
2022-05-22 15:20:13 -0500

    add multi output model

--- keras_transformer/transformer.py
+++ keras_transformer/transformer.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -410,8 +412,184 @@ def get_model(token_num,
     )([decoded_layer, decoder_embed_weights])
     return keras.models.Model(inputs=[encoder_input, decoder_input], outputs=output_layer)
 
+def get_multi_output_model(token_num,
+              embed_dim,
+              encoder_num,
+              decoder_num,
+              head_num,
+              hidden_dim,
+              attention_activation=None,
+              feed_forward_activation=gelu,
+              dropout_rate=0.0,
+              use_same_embed=True,
+              embed_weights=None,
+              embed_trainable=None,
+              trainable=True):
+    """Get full model without compilation.
+       :param token_num: Number of distinct tokens.
+       :param embed_dim: Dimension of token embedding.
+       :param encoder_num: Number of encoder components.
+       :param decoder_num: Number of decoder components.
+       :param head_num: Number of heads in multi-head self-attention.
+       :param hidden_dim: Hidden dimension of feed forward layer.
+       :param attention_activation: Activation for multi-head self-attention.
+       :param feed_forward_activation: Activation for feed-forward layer.
+       :param dropout_rate: Dropout rate.
+       :param use_same_embed: Whether to use the same token embedding layer. `token_num`, `embed_weights` and
+                              `embed_trainable` should be lists of two elements if it is False.
+       :param embed_weights: Initial weights of token embedding.
+       :param embed_trainable: Whether the token embedding is trainable. It will automatically set to False if the given
+                               value is None when embedding weights has been provided.
+       :param trainable: Whether the layers are trainable.
+       :return: Keras model.
+       """
+    if not isinstance(token_num, list):
+        token_num = [token_num, token_num]
+    encoder_token_num, decoder_token_num = token_num
 
-def get_multi_model(token_num,
+    if not isinstance(embed_weights, list):
+        embed_weights = [embed_weights, embed_weights]
+    encoder_embed_weights, decoder_embed_weights = embed_weights
+    if encoder_embed_weights is not None:
+        encoder_embed_weights = [encoder_embed_weights]
+    if decoder_embed_weights is not None:
+        decoder_embed_weights1 = copy.deepcopy(decoder_embed_weights)
+        decoder_embed_weights2 = copy.deepcopy(decoder_embed_weights)
+        decoder_embed_weights3 = copy.deepcopy(decoder_embed_weights)
+        decoder_embed_weights4 = copy.deepcopy(decoder_embed_weights)
+        decoder_embed_weights5 = copy.deepcopy(decoder_embed_weights)
+
+    if not isinstance(embed_trainable, list):
+        embed_trainable = [embed_trainable, embed_trainable]
+    encoder_embed_trainable, decoder_embed_trainable = embed_trainable
+    if encoder_embed_trainable is None:
+        encoder_embed_trainable = encoder_embed_weights is None
+    if decoder_embed_trainable is None:
+        decoder_embed_trainable = decoder_embed_weights is None
+
+    if use_same_embed:
+        encoder_embed_layer = decoder_embed_layer1 = decoder_embed_layer2 = decoder_embed_layer3 = decoder_embed_layer4 = decoder_embed_layer5 = EmbeddingRet(
+            input_dim=encoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=encoder_embed_weights,
+            trainable=encoder_embed_trainable,
+            name='Token-Embedding',
+        )
+    else:
+        encoder_embed_layer = EmbeddingRet(
+            input_dim=encoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=encoder_embed_weights,
+            trainable=encoder_embed_trainable,
+            name='Encoder-Token-Embedding',
+        )
+        decoder_embed_layer1 = EmbeddingRet(
+            input_dim=decoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=decoder_embed_weights1,
+            trainable=decoder_embed_trainable,
+            name='Decoder-Token-Embedding',
+        )
+        decoder_embed_layer2 = EmbeddingRet(
+            input_dim=decoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=decoder_embed_weights2,
+            trainable=decoder_embed_trainable,
+            name='Decoder-Token-Embedding',
+        )
+        decoder_embed_layer3 = EmbeddingRet(
+            input_dim=decoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=decoder_embed_weights3,
+            trainable=decoder_embed_trainable,
+            name='Decoder-Token-Embedding',
+        )
+        decoder_embed_layer4 = EmbeddingRet(
+            input_dim=decoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=decoder_embed_weights4,
+            trainable=decoder_embed_trainable,
+            name='Decoder-Token-Embedding',
+        )
+        decoder_embed_layer5 = EmbeddingRet(
+            input_dim=decoder_token_num,
+            output_dim=embed_dim,
+            mask_zero=True,
+            weights=decoder_embed_weights5,
+            trainable=decoder_embed_trainable,
+            name='Decoder-Token-Embedding',
+        )
+    encoder_input = keras.layers.Input(shape=(None,), name='Encoder-Input')
+    encoder_embed = TrigPosEmbedding(
+        mode=TrigPosEmbedding.MODE_ADD,
+        name='Encoder-Embedding',
+    )(encoder_embed_layer(encoder_input)[0])
+    encoded_layer = get_encoders(
+        encoder_num=encoder_num,
+        input_layer=encoder_embed,
+        head_num=head_num,
+        hidden_dim=hidden_dim,
+        attention_activation=attention_activation,
+        feed_forward_activation=feed_forward_activation,
+        dropout_rate=dropout_rate,
+        trainable=trainable,
+    )
+    decoder_input1 = keras.layers.Input(shape=(None,), name='Decoder-Input')
+    decoder_input2 = keras.layers.Input(shape=(None,), name='Decoder-Input')
+    decoder_input3 = keras.layers.Input(shape=(None,), name='Decoder-Input')
+    decoder_input4 = keras.layers.Input(shape=(None,), name='Decoder-Input')
+    decoder_input5 = keras.layers.Input(shape=(None,), name='Decoder-Input')
+    decoder_embed1, decoder_embed_weights1 = decoder_embed_layer1(decoder_input1)
+    decoder_embed2, decoder_embed_weights2 = decoder_embed_layer2(decoder_input2)
+    decoder_embed3, decoder_embed_weights3 = decoder_embed_layer3(decoder_input3)
+    decoder_embed4, decoder_embed_weights4 = decoder_embed_layer4(decoder_input4)
+    decoder_embed5, decoder_embed_weights5 = decoder_embed_layer5(decoder_input5)
+    decoder_embed = keras.layers.Concatenate([decoder_embed1, decoder_embed2, decoder_embed3, decoder_embed4, decoder_embed5])
+    decoder_embed = TrigPosEmbedding(
+        mode=TrigPosEmbedding.MODE_ADD,
+        name='Decoder-Embedding',
+    )(decoder_embed)
+    decoded_layer = get_decoders(
+        decoder_num=decoder_num,
+        input_layer=decoder_embed,
+        encoded_layer=encoded_layer,
+        head_num=head_num,
+        hidden_dim=hidden_dim,
+        attention_activation=attention_activation,
+        feed_forward_activation=feed_forward_activation,
+        dropout_rate=dropout_rate,
+        trainable=trainable,
+    )
+    output_layer1 = EmbeddingSim(
+        trainable=trainable,
+        name='Decoder-Output1',
+    )([decoded_layer, decoder_embed_weights1])
+    output_layer2 = EmbeddingSim(
+        trainable=trainable,
+        name='Decoder-Output2',
+    )([decoded_layer, decoder_embed_weights2])
+    output_layer3 = EmbeddingSim(
+        trainable=trainable,
+        name='Decoder-Output3',
+    )([decoded_layer, decoder_embed_weights3])
+    output_layer4 = EmbeddingSim(
+        trainable=trainable,
+        name='Decoder-Output4',
+    )([decoded_layer, decoder_embed_weights4])
+    output_layer5 = EmbeddingSim(
+        trainable=trainable,
+        name='Decoder-Output5',
+    )([decoded_layer, decoder_embed_weights5])
+    return keras.models.Model(inputs=[encoder_input, decoder_input1, decoder_input2, decoder_input3, decoder_input4, decoder_input5], outputs=[output_layer1, output_layer2, output_layer3, output_layer4, output_layer5])
+
+
+def get_multi_input_model(token_num,
               embed_dim,
               encoder_num,
               decoder_num,
